1f0410bf0c36dfa43af18a7f488fd0facbc01790
MooCow <DannyCork@users.noreply.github.com>
2021-03-19 14:40:22 +0000

    Update __init__.py

--- whois/__init__.py
+++ whois/__init__.py
@@ -7,8 +7,8 @@
     Usage example
     >>> import whois
     >>> domain = whois.query('google.com')
-
     >>> print(domain.__dict__)
+    
     {'expiration_date': datetime.datetime(2020, 9, 14, 0, 0), 'last_updated': datetime.datetime(2011, 7, 20, 0, 0), 'registrar': 'MARKMONITOR INC.', 'name': 'google.com', 'creation_date': datetime.datetime(1997, 9, 15, 0, 0)}
 
     >>> print(domain.name)
