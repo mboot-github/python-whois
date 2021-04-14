6a894dfcc5ecdf927ec6da22be19f5c5e9df35ba
Wouter Kobes <w.j.kobes@student.utwente.nl>
2021-04-14 18:40:27 +0200

    Adds new TLDs by request

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='whois',
-    version='0.9.8',
+    version='0.9.10',
     description='Python package for retrieving WHOIS information of domains.',
     author='DannyCork',
     author_email='ddarko@ddarko.org',
     license='MIT http://www.opensource.org/licenses/mit-license.php',
-    download_url='https://github.com/DannyCork/python-whois/releases/tag/0.8',
+    download_url='https://github.com/DannyCork/python-whois/releases/tag/0.9.10',
     url='https://github.com/DannyCork/python-whois/',
     platforms=['any'],
     packages=['whois'],
