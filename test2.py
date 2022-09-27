d7aef409b3497cccd3303a117732c1cf075d1ddd
ReversingLabs <55623149+rl-devops@users.noreply.github.com>
2022-09-27 12:09:20 +0200

    getops for test2.py

--- test2.py
+++ test2.py
@@ -5,8 +5,8 @@ import re
 import getopt
 import sys
 
-Verbose = True
-failure = {}
+Verbose = False
+Failures = {}
 
 
 def prepItem(d):
@@ -32,9 +32,9 @@ def testItem(d):
 
 
 def errorItem(d, e, what="Generic"):
-    if what not in failure:
-        failure[what] = {}
-    failure[what][d] = e
+    if what not in Failures:
+        Failures[what] = {}
+    Failures[what][d] = e
 
     message = f"Domain: {d}; Exception: {what}; Error: {e}"
     print(message)
@@ -77,33 +77,34 @@ def testDomains(aList):
             errorItem(d, e, what="Generic")
 
 
-def getTestFilesAll(tDir):
-    result = {}
+def getTestFileOne(fPath, fileData):
+    if not os.path.isfile(fPath):  # only files
+        return
 
+    if not fPath.endswith(".txt"):  # ending in .txt
+        return
+
+    bName = fPath[:-4]
+    fileData[bName] = []
+    xx = fileData[bName]
+
+    with open(fPath) as f:
+        for index, line in enumerate(f):
+            line = line.strip()
+            if len(line) == 0 or line.startswith("#"):
+                continue
+
+            aa = re.split(r"\s+", line)
+            if aa[0] not in xx:
+                xx.append(aa[0])
+
+    return
+
+
+def getTestFilesAll(tDir, fileData):
     for item in os.listdir(tDir):
         fPath = f"{tDir}/{item}"
-
-        if not os.path.isfile(fPath):  # only files
-            continue
-
-        if not item.endswith(".txt"):  # ending in .txt
-            continue
-
-        bName = item[:-4]
-        result[bName] = []
-        xx = result[bName]
-
-        with open(fPath) as f:
-            for index, line in enumerate(f):
-                line = line.strip()
-                if len(line) == 0 or line.startswith("#"):
-                    continue
-
-                aa = re.split(r"\s+", line)
-                if aa[0] not in xx:
-                    xx.append(aa[0])
-
-    return result
+        getTestFileOne(fPath, fileData)
 
 
 def getAllCurrentTld():
@@ -124,65 +125,149 @@ def showAllCurrentTld():
 
 
 def usage():
-    print("test.py -i <inputfile> -o <outputfile>")
+    print(
+        """
+test.py
+    [ -v | --verbose ]
+        # set verbose to True, this will be forwarded to whois.query
 
-    """ TODO
-    --all
-        tld supported [default]
+    [ -a | --all]
+        # test all existing tld currently supported,
 
+    [ -d <domain> | --domain = <domain> " ]
+        # only analyze the given domains
+        # the option can be repeated to specify more then one domain
+
+    [ -f <filename> | --file = <filename> " ]
+        # use the named file to test all domains (one domain per line)
+        # lines starting with # or empty lines are skipped, anything after the domain is ignored
+        # the option can be repeated to specify more then one file
+
+    [ -D <directory> | --Directory = <directory> " ]
+        # use the named directory, ald use all files ending in .txt as files containing domains
+        # files are processed as in the -f option so comments and empty lines are skipped
+        # the option can be repeated to specify more then one directory
+
+    example: ./test2.py -f tests/ok-domains.txt -D tests -d meta.org -d meta.com -a 2>2 >out
+"""
+    )
+
+    """
+    TODO
     --all --reg <re>
         from all tld a regex match sub selection
 
     --all -- having <name>
         from all but only the ones haveing a certain field
-
-    -- domain , -d
-        only one domain
-
-    -- file, -f
-        only one file
-
-    -- directory, -D
-        only one dir
-
     """
 
 
+def showFailures():
+    if len(Failures):
+        print("\n# ========================")
+        for i in sorted(Failures.keys()):
+            for j in sorted(Failures[i].keys()):
+                print(i, j, Failures[i][j])
+
+
 def main(argv):
-    tDir = "tests"
-    testAllTld = True
-    testAllFiles = False
+    global Verbose
 
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile="])
+        opts, args = getopt.getopt(
+            argv,
+            "vhaf:d:D:",
+            [
+                "verbose",
+                "all",
+                "file =",
+                "Directory =",
+                "domain =",
+            ],
+        )
     except getopt.GetoptError:
         usage()
         sys.exit(2)
+
+    testAllTld = False
+
+    directory = None
+    dirs = []
+
+    filename = None
+    files = []
+
+    domain = None
+    domains = []
+
+    fileData = {}
 
     for opt, arg in opts:
         if opt == "-h":
             usage()
             sys.exit()
 
-        if opt in ("-i", "--ifile"):
-            inputfile = arg
+        if opt in ("-a", "--all"):
+            testAllTld = True
 
-        if opt in ("-o", "--ofile"):
-            outputfile = arg
+        if opt in ("-v", "--verbose"):
+            Verbose = True
+
+        if opt in ("-D", "--Directory"):
+            directory = arg
+            isDir = os.path.isdir(directory)
+            if isDir is False:
+                print(f"{directory} cannot be found or is not a directory", file=sys.stderr)
+                sys.exit(101)
+
+            if directory not in dirs:
+                dirs.append(directory)
+                testAllTld = False
+
+        if opt in ("-f", "--file"):
+            filename = arg
+            isFile = os.path.isfile(filename)
+            if isFile is False:
+                print(f"{filename} cannot be found or is not a file", file=sys.stderr)
+                sys.exit(101)
+
+            if filename not in files:
+                files.append(filename)
+                testAllTld = False
+
+        if opt in ("-d", "--domain"):
+            domain = arg
+            if domain not in domains:
+                domains.append(domain)
 
     if testAllTld:
+        print("## ===== TEST CURRENT TLD's")
         allMetaTld = makeMateAllCurrentTld()
         testDomains(allMetaTld)
+        showFailures()
 
-    if testAllFiles:
-        result = getTestFilesAll(tDir)
-        for testFile in result:
-            testDomains(result[testFile])
+    if len(dirs):
+        print("## ===== TEST DIRECTORIES")
+        for dName in dirs:
+            getTestFilesAll(dName, fileData)
+        for testFile in fileData:
+            print(f"## ===== TEST FILE: {testFile}")
+            testDomains(fileData[testFile])
+        showFailures()
 
-    print("\n# ========================")
-    for i in sorted(failure.keys()):
-        for j in sorted(failure[i].keys()):
-            print(i, j, failure[i][j])
+    if len(files):
+        print("## ===== TEST FILES")
+        for testFile in files:
+            getTestFileOne(testFile, fileData)
+        for testFile in fileData:
+            print(f"## ===== TEST FILE: {testFile}")
+            testDomains(fileData[testFile])
+        showFailures()
+
+    if len(domains):
+        print("## ===== TEST DOMAINS")
+        testDomains(domains)
+        showFailures()
 
 
 if __name__ == "__main__":
