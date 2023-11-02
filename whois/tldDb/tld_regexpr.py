036970495cad61404334e94e20bb1ec2bd572d32
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-11-02 11:24:00 +0100

    comment

--- whois/tldDb/tld_regexpr.py
+++ whois/tldDb/tld_regexpr.py
@@ -735,6 +735,8 @@ ZZ["pl"] = {
     "registrar": R(r"\nREGISTRAR:\s*(.+)\n"),
     "creation_date": R(r"\ncreated:\s*(.+)\n"),
     "updated_date": R(r"\nlast modified:\s*(.+)\n"),
+    # If no "option expiration date:" is present, use "renewal date:", but only
+    # if it's not not followed by "option expiration date:".
     "expiration_date": R(r"(?:\noption expiration date:|renewal date:(?!(?:.|\n)*\noption expiration date:))\s*(.+)\n"),
     "name_servers": R(r"nameservers:%s" % xStr(r"(?:\s+(\S+)[^\n]*\n)?", 4)),
     "status": R(r"\nStatus:\n\s*(.+)"),
